e81ee961bbdd3ac1762f7616950aed427f0aa11c
Nikoleta Koleva <nvkoleva@yahoo.com>
2021-02-18 21:34:42 -0500

    section 3 notes & etc.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,24 @@
+import React from 'react';
+import './Person.css'
+
 // ES6 equivalent to JS function -> this is an arrow function
-const person = () => {
-    
-}
+
+// props = all the properties of the components/attributes u add to each object
+//  -> they're passed as if they're function parameters
+const person = ( props ) => {
+    // need 2 import react cuz we're using JSX
+    // Can execute 1-line expressions in {} running dynamic React content
+    // return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+        
+    )
+};
+
+// This component is reusable!
+
+export default person;
